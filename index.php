f7377d2b50f844e0e25269e85d683f50aa444d27
Fatemeh <fatemeh.gharri@yahoo.com>
2020-05-30 12:55:34 +0200

    Create base structure

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fatemeh
+ * Date: 5/30/2020
+ * Time: 12:51 PM
+ */
---
+++ src/App/Application.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fatemeh
+ * Date: 5/30/2020
+ * Time: 12:52 PM
+ */
+
+class Application
+{
+
+}
