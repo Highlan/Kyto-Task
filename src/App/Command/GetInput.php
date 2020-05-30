7f71b342c6a0f749f63db149d00e2ab39b2cd588
Fatemeh <fatemeh.gharri@yahoo.com>
2020-05-30 14:49:20 +0200

    Prepare input height for cli agent

--- src/App/Command/GetInput.php
+++ src/App/Command/GetInput.php
@@ -10,6 +10,24 @@ class GetInput implements IInput
 
     public function get()
     {
-        // TODO: Implement get() method.
+        return $this->execute();
+    }
+
+    public function execute()
+    {
+        while (true)
+        {
+            echo "Please choose height among these values (" . implode(', ', HEIGHT_ARRAY_SIZE) . ")? ";
+
+            $input = (int)rtrim(fgets(STDIN));
+            if (in_array($input, HEIGHT_ARRAY_SIZE))
+            {
+                return $input;
+            }
+            else
+            {
+                echo PHP_EOL . "Invalid Input. Please try again." . PHP_EOL;
+            }
+        }
     }
 }
